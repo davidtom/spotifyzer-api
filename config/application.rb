986c791b7e1ec0e6efe4f30b9924f8cae231bd1c
davidtom <davidjtomczyk@gmail.com>
2017-09-03 16:02:33 -0400

    add rails rack-cors

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,13 @@ module SpotifyzerApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # rack-cors config - https://github.com/cyu/rack-cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+end
+
   end
 end
