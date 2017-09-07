d74afe1c5c18fd2fb88aece6aa5e026b0f3b2eb0
davidtom <davidjtomczyk@gmail.com>
2017-09-07 12:03:32 -0400

    profile images are now updated with each login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,8 +26,9 @@ class Api::V1::UsersController < ApplicationController
                       spotify_url: user_params["external_urls"]["spotify"],
                       href: user_params["href"],
                       uri: user_params["uri"])
-    # Add user's profile image, if it exists:
-    @user.update(profile_img_url: user_params["images"][0]["url"]) if user_params["images"][0]
+    # Add or update user's profile image:
+    img_url = user_params["images"][0] ? user_params["images"][0]["url"] : nil
+    @user.update(profile_img_url: img_url)
     # Update the access and refresh tokens in the database
     @user.update(access_token:auth_params["access_token"], refresh_token: auth_params["refresh_token"])
 
