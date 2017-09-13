4b85eeb9a04b99ccfaca5e7b38cc5d203077881f
davidtom <davidjtomczyk@gmail.com>
2017-09-13 15:04:52 -0400

    fix indentation

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::TracksController < ApplicationController
   end
 
   def top
-   time_range = params[:time_range] || "medium_term"
-   top_tracks = SpotifyAPIAdapter.get_user_top_tracks(current_user, time_range)
-   render json: top_tracks, status: 200
+    time_range = params[:time_range] || "medium_term"
+    top_tracks = SpotifyAPIAdapter.get_user_top_tracks(current_user, time_range)
+    render json: top_tracks, status: 200
   end
 
   def recent
